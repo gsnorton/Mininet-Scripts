40bb780389fea4d659eb27f71581c952d573b301
Gregory Norton <gregnorton@gmail.com>
2014-04-15 13:03:28 -0700

    Tweaked. Now working in lab environment.

--- HostInternet.py
+++ HostInternet.py
@@ -17,7 +17,7 @@ def myNetwork():
     info( '*** Add switches\n')
     s1 = net.addSwitch('s1')
     Intf( 'eth1', node=s1 )
-    Intf( 'tap0', node=s1 )
+    # Intf( 'tap0', node=s1 )
 
     info( '*** Add hosts\n')
     h1 = net.addHost('h1', ip='0.0.0.0')
